6353a51cfa821c714480c6ada43160639e4277a3
J1nH@n <56214632+robin789789@users.noreply.github.com>
2020-10-28 17:45:52 +0800

    Initial commit

---
+++ PlatformHeightTest/PlatformHeightTest.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PlatformHeightTest
+{
+    partial class PlatformHeightTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FilePath = new System.Windows.Forms.Label();
+            this.WatchPathBtn = new System.Windows.Forms.Button();
+            this.ScaleCB = new System.Windows.Forms.CheckBox();
+            this.LastUpdateTime = new System.Windows.Forms.Label();
+            this.StopWatchBtn = new System.Windows.Forms.Button();
+            this.checkWatch = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.OrisModecheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FilePath
+            // 
+            this.FilePath.AutoSize = true;
+            this.FilePath.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FilePath.ForeColor = System.Drawing.Color.Red;
+            this.FilePath.Location = new System.Drawing.Point(8, 46);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Size = new System.Drawing.Size(46, 22);
+            this.FilePath.TabIndex = 1;
+            this.FilePath.Text = "path";
+            // 
+            // WatchPathBtn
+            // 
+            this.WatchPathBtn.Location = new System.Drawing.Point(9, 10);
+            this.WatchPathBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.WatchPathBtn.Name = "WatchPathBtn";
+            this.WatchPathBtn.Size = new System.Drawing.Size(96, 32);
+            this.WatchPathBtn.TabIndex = 2;
+            this.WatchPathBtn.Text = "Watch";
+            this.WatchPathBtn.UseVisualStyleBackColor = true;
+            this.WatchPathBtn.Click += new System.EventHandler(this.WatchPathBtn_Click);
+            // 
+            // ScaleCB
+            // 
+            this.ScaleCB.AutoSize = true;
+            this.ScaleCB.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ScaleCB.Location = new System.Drawing.Point(81, 74);
+            this.ScaleCB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ScaleCB.Name = "ScaleCB";
+            this.ScaleCB.Size = new System.Drawing.Size(96, 23);
+            this.ScaleCB.TabIndex = 4;
+            this.ScaleCB.Text = "um / mm";
+            this.ScaleCB.UseVisualStyleBackColor = true;
+            // 
+            // LastUpdateTime
+            // 
+            this.LastUpdateTime.AutoSize = true;
+            this.LastUpdateTime.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.LastUpdateTime.Location = new System.Drawing.Point(8, 109);
+            this.LastUpdateTime.Name = "LastUpdateTime";
+            this.LastUpdateTime.Size = new System.Drawing.Size(93, 19);
+            this.LastUpdateTime.TabIndex = 5;
+            this.LastUpdateTime.Text = "update time";
+            // 
+            // StopWatchBtn
+            // 
+            this.StopWatchBtn.Location = new System.Drawing.Point(282, 10);
+            this.StopWatchBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.StopWatchBtn.Name = "StopWatchBtn";
+            this.StopWatchBtn.Size = new System.Drawing.Size(96, 32);
+            this.StopWatchBtn.TabIndex = 6;
+            this.StopWatchBtn.Text = "Stop Watch";
+            this.StopWatchBtn.UseVisualStyleBackColor = true;
+            this.StopWatchBtn.Click += new System.EventHandler(this.StopWatchBtn_Click);
+            // 
+            // checkWatch
+            // 
+            this.checkWatch.AutoSize = true;
+            this.checkWatch.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkWatch.Location = new System.Drawing.Point(197, 74);
+            this.checkWatch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkWatch.Name = "checkWatch";
+            this.checkWatch.Size = new System.Drawing.Size(132, 23);
+            this.checkWatch.TabIndex = 7;
+            this.checkWatch.Text = "check Watcher";
+            this.checkWatch.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Z",
+            "S"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 74);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(55, 23);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // OrisModecheckBox
+            // 
+            this.OrisModecheckBox.AutoSize = true;
+            this.OrisModecheckBox.Checked = true;
+            this.OrisModecheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.OrisModecheckBox.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.OrisModecheckBox.Location = new System.Drawing.Point(121, 15);
+            this.OrisModecheckBox.Name = "OrisModecheckBox";
+            this.OrisModecheckBox.Size = new System.Drawing.Size(140, 23);
+            this.OrisModecheckBox.TabIndex = 8;
+            this.OrisModecheckBox.Text = "Easy Oris Mode";
+            this.OrisModecheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(364, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Average:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(362, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Max:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(362, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Min:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(362, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Tolerance:";
+            // 
+            // PlatformHeightTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 480);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OrisModecheckBox);
+            this.Controls.Add(this.checkWatch);
+            this.Controls.Add(this.StopWatchBtn);
+            this.Controls.Add(this.LastUpdateTime);
+            this.Controls.Add(this.ScaleCB);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.WatchPathBtn);
+            this.Controls.Add(this.FilePath);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PlatformHeightTest";
+            this.ShowIcon = false;
+            this.Text = "HeightTest";
+            this.Load += new System.EventHandler(this.PlatformHeightTest_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label FilePath;
+        private System.Windows.Forms.Button WatchPathBtn;
+        private System.Windows.Forms.CheckBox ScaleCB;
+        private System.Windows.Forms.Label LastUpdateTime;
+        private System.Windows.Forms.Button StopWatchBtn;
+        private System.Windows.Forms.CheckBox checkWatch;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox OrisModecheckBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ PlatformHeightTest/PlatformHeightTest.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+
+namespace PlatformHeightTest
+{
+    public partial class PlatformHeightTest : Form
+    {
+        public PlatformHeightTest()
+        {
+            InitializeComponent();
+            createButtons();
+        }
+
+        #region Announce
+        private FileSystemWatcher watcher;
+        private const string v2kPathFolder = @".\HeightRecognition";
+        private double avg = 0;
+        private double[] data = new double[9];
+        private int[] way = new int[9];//Z+S        
+        private string lastData = "";
+        #endregion
+
+        private void PlatformHeightTest_Load(object sender, EventArgs e)
+        {
+            Button.CheckForIllegalCrossThreadCalls = false;
+            CenterToScreen();
+            comboBox1.SelectedIndex = 0;
+            try
+            {
+                startWatchHeight(v2kPathFolder);
+                FilePath.Text = v2kPathFolder;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + "請手動添加測高資料夾位址", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void WatchPathBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowNewFolderButton = false;
+            fbd.RootFolder = Environment.SpecialFolder.DesktopDirectory;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                startWatchHeight(fbd.SelectedPath);
+                FilePath.Text = fbd.SelectedPath;
+            }
+        }//choose folder to watch
+
+        private void startWatchHeight(string watchFolder)
+        {
+            watcher = new FileSystemWatcher
+            {
+                Path = watchFolder,
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName,
+                Filter = "*.csv",
+                IncludeSubdirectories = false
+            };
+            watcher.Changed += _Watch_Changed;
+            watcher.Error += _Watch_Error;
+            watcher.EnableRaisingEvents = true;
+        } //start watch heightTest folder
+
+        private void StopWatchBtn_Click(object sender, EventArgs e)
+        {
+            if (watcher != null)
+            {
+                watcher.EnableRaisingEvents = false;
+                watcher.Dispose();
+            }
+        }//stop the watcher 
+
+        private void _Watch_Changed(object sender, FileSystemEventArgs e)//if change heighttest
+        {
+            var dirInfo = new DirectoryInfo(e.FullPath);
+            if (checkWatch.Checked)
+            {
+                MessageBox.Show("被更改內容的檔名為" + e.Name
+                    + Environment.NewLine
+                    + "檔案所在位址為" + e.FullPath.Replace(e.Name, "")
+                    , "數據更新通知", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            LastUpdateTime.Text = "最後更新:" + dirInfo.LastWriteTime;
+            Thread.Sleep(1000);
+            lastData = File.ReadLines(e.FullPath).Last();
+
+            data = results(lastData);
+            if (data != null)
+            {
+                avg = getAvg(data);
+                label2.Text = "Max: " + getMaxMin(data, "Max").ToString()+" mm" ;
+                label3.Text = "Min: " + getMaxMin(data ,"Min").ToString()+" mm";
+                label4.Text = "Tolerance: " + (getMaxMin(data, "Max")-getMaxMin(data, "Min")).ToString()+ " mm";
+                foreach (Control bt in this.Controls)
+                {
+                    if (bt is Button)
+                    {
+                        if (bt.Name != "WatchPathBtn" && bt.Name != "StopWatchBtn")
+                        {
+                            int index = int.Parse(bt.Name);
+                            bt.Text = compareToAvg(data[way[index] - 1], avg, ScaleCB.Checked,OrisModecheckBox.Checked);
+                            
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("測高數據異常，請確認為九點測高", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void _Watch_Error(object sender, ErrorEventArgs e)
+        {
+            MessageBox.Show("請重新選擇測高資料夾.", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }//exception     
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            way = ZS(comboBox1.Text);
+            if (watcher != null)
+            {
+                dataChanged();
+            }
+            else
+            {
+                foreach (Control bt in this.Controls)
+                {
+                    if (bt is Button)
+                    {
+                        if (bt.Name != "WatchPathBtn" && bt.Name != "StopWatchBtn")
+                        {
+                            bt.Text = "第 " + way[int.Parse(bt.Name)].ToString() + " 點";
+                        }
+                    }
+                }
+            }
+        }
+
+        #region Function
+
+        private string getTimeFormatCsvFile()
+        {
+            DateTime time = DateTime.Now;
+            string[] t = time.Date.ToShortDateString().Split('/');
+            string format = "";
+            if (int.Parse(t[1]) < 10)
+            {
+                t[1] = "0" + t[1];
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                format += t[i];
+            }
+            format = "HeightRecognition_" + format + ".csv";
+            return format;
+        } //抓日期格式
+
+        private string lastLine(string path) //讀路徑抓最後行
+        {
+            string line;
+            string[] each;
+            StreamReader streamReader = new StreamReader(path);
+            string all = streamReader.ReadToEnd();
+            each = all.Split(new string[1] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            line = each[each.Length - 1];
+            streamReader.Close();
+            return line;
+        }
+
+        private double[] results(string latestData)//最後行轉九點高度
+        {
+            double[] results;
+            string[] buf = latestData.Split(',');
+            results = new double[buf.Length - 5];
+            try
+            {
+                for (int i = 5; i < buf.Length; i++)
+                {
+                    results[i - 5] = double.Parse(buf[i]);
+                }
+                return results;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private double getAvg(double[] data)//輸入九點轉平均
+        {
+            int l = data.Length;
+            double d = 0;
+            for (int i = 0; i < l; i++)
+            {
+                d += data[i];
+            }
+            return d / l;
+        }
+
+        private string compareToAvg(double r, double avg, bool scale,bool OrisMode)
+        {
+            int _um;
+            string _scale;
+            if (scale)
+            {
+                _um = 1000;
+                _scale = " um";
+            }
+            else
+            {
+                _um = 1;
+                _scale = " mm";
+            }
+            if (!OrisMode)
+            {
+                double result = r - avg;
+                int d = result.ToString().Length;
+                if (result > 0)
+                {
+                    if (d > 6)
+                        return "低" + (result * _um).ToString().Remove(6) + _scale;
+                    else
+                        return "低" + (result * _um).ToString() + _scale;
+                }
+                else
+                {
+                    if (d > 6)
+                        return "高" + (result * _um).ToString().Remove(6) + _scale;
+                    else
+                        return "高" + (result * _um).ToString() + _scale;
+                }
+            }
+            else
+            {
+                label1.Text = ("Average: " + (avg*_um).ToString()+_scale);
+               
+                return (r * _um).ToString()+ _scale;
+            }
+        }
+
+        private void createButtons()
+        {
+            Button[] bt = new Button[9];
+            int size = 80;
+            int[] location = new int[2] { 10, 120 };
+            int gap = 5;
+
+            for (int i = 0; i < bt.Length; i++)
+            {
+                bt[i] = new Button();
+                bt[i].Name = i.ToString();
+                bt[i].Size = new Size(size, size);
+                if (i < 3)
+                    bt[i].Location = new Point(location[0] + i * (gap + size), location[1]);
+                if (i >= 3 && i < 6)
+                    bt[i].Location = new Point(location[0] + (i - 3) * (gap + size), location[1] + size + gap);
+                if (i >= 6)
+                    bt[i].Location = new Point(location[0] + (i - 6) * (gap + size), location[1] + 2 * (size + gap));
+                this.Controls.Add(bt[i]);
+            }
+        }
+
+        private void dataChanged()
+        {
+            foreach (Control bt in this.Controls)
+            {
+                if (bt is Button)
+                {
+                    if (bt.Name != "WatchPathBtn" && bt.Name != "StopWatchBtn")
+                    {
+                        int index = int.Parse(bt.Name);
+                        bt.Text = compareToAvg(data[way[index] - 1], avg, ScaleCB.Checked, OrisModecheckBox.Checked);
+                    }
+                }
+            }
+        }
+
+        private int[] ZS(string type)
+        {
+            if (type == "Z")
+            {
+                int[] results = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                return results;
+            }
+            if (type == "S")
+            {
+                int[] results = new int[9] { 1, 2, 3, 6, 5, 4, 7, 8, 9 };
+                return results;
+            }
+            return null;
+        }
+
+        private double getMaxMin(double[] datastring, string MaxorMin)
+        {
+            double answer = 0, temp = 0;
+            double[] sortarray = new double[4];
+
+            sortarray[0] = datastring[0];
+            sortarray[1] = datastring[2];
+            sortarray[2] = datastring[6];
+            sortarray[3] = datastring[8];// edge 4 points
+            Array.Sort(sortarray);
+
+            if (MaxorMin == "Min")
+            {
+                temp = sortarray[0];
+                answer = temp;
+            }
+            else if (MaxorMin == "Max")
+            {
+                temp = sortarray[sortarray.Length - 1];
+                answer = temp;
+            }
+            return answer;
+        }
+
+        #endregion
+
+       
+    }
+}
