a8009bf3cf057a16e1876446d4235e6d21cf3476
J1nH@n <56214632+robin789789@users.noreply.github.com>
2021-04-08 14:09:30 +0800

    update AssenbkyInfo.cs 3.1.0.0

--- PlatformHeightTest/Properties/AssemblyInfo.cs
+++ PlatformHeightTest/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // 設為預設，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
